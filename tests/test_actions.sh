8a9e8263c9abf764a1a68a50b9dce916eade7f3f
Peter Forret <peter@forret.com>
2022-04-03 22:10:39 +0200

    redo github action for bash_unit

--- tests/test_actions.sh
+++ tests/test_actions.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 # test functions should start with test_
 # using https://github.com/pgrange/bash_unit
-#  fail
-#  assert
-#  assert "test -e /tmp/the_file"
-#  assert_fails "grep this /tmp/the_file" "should not write 'this' in /tmp/the_file"
-#  assert_status_code 25 code
-#  assert_equals "a string" "another string" "a string should be another string"
-#  assert_not_equals "a string" "a string" "a string should be different from another string"
-#  fake ps echo hello world
 
 root_folder=$(cd .. && pwd) # tests/.. is root folder
 # shellcheck disable=SC2012
 # shellcheck disable=SC2035
 root_script=$(find "$root_folder"  -maxdepth 1 -name "*.sh" | head -1) # normally there should be only 1
-
 
 test_lower() {
   assert_equals "all is lower" "$(<<< "ALL IS LOWER" "$root_script" lower)"
@@ -47,5 +38,3 @@ test_slugify(){
 test_ascii(){
     assert_equals "International" "$(<<< "Îñtérñåtìôńāl" "$root_script" ascii)"
 }
-
-
--- tests/test_basic.sh
+++ tests/test_basic.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 # test functions should start with test_
 # using https://github.com/pgrange/bash_unit
-#  fail
-#  assert
-#  assert "test -e /tmp/the_file"
-#  assert_fails "grep this /tmp/the_file" "should not write 'this' in /tmp/the_file"
-#  assert_status_code 25 code
-#  assert_equals "a string" "another string" "a string should be another string"
-#  assert_not_equals "a string" "a string" "a string should be different from another string"
-#  fake ps echo hello world
 
 root_folder=$(cd .. && pwd) # tests/.. is root folder
 # shellcheck disable=SC2012
