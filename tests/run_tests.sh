99c309d2f0d6d54c51a3b7a69a373d9589b8db10
Peter Forret <peter@forret.com>
2022-04-03 22:12:47 +0200

    MOD: run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,7 +2,6 @@
 script_path=$(dirname "$0")
 script_path=$(cd -P "$script_path" && pwd)
 
-clear
 if [[ ! -x "$(command -v bash_unit)" ]] ; then
   ## used by Github action or if bash_unit is not installed
   curl -s "https://raw.githubusercontent.com/pgrange/bash_unit/master/bash_unit" -o "$script_path/bash_unit"
