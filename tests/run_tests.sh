f1c55ac72a00550ea2ec676d94a2d23edb66a798
Peter Forret <peter@forret.com>
2022-04-03 22:38:55 +0200

    MOD: run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,11 +2,15 @@
 script_path=$(dirname "$0")
 script_path=$(cd -P "$script_path" && pwd)
 
-if [[ ! -x "$(command -v bash_unit)" ]] ; then
+if [[ -x "$(command -v bash_unit)" ]] ; then
+  ## when bash_unit is installed globally
+  cd "$script_path" && bash_unit -f tap test_*
+elif [[ -x "$script_path/bash_unit" ]] ; then
+  ## when bash_unit is already installed in the folder
+  cd "$script_path" && "$script_path/bash_unit" -f tap test_*
+else
   ## used by Github action or if bash_unit is not installed
   curl -s "https://raw.githubusercontent.com/pgrange/bash_unit/master/bash_unit" -o "$script_path/bash_unit"
   chmod +x "$script_path/bash_unit"
   cd "$script_path" && "$script_path/bash_unit" -f tap test_*
-else
-  cd "$script_path" && bash_unit -f tap test_*
 fi
